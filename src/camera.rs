f9fa7dcb3c9c44a2e9e6cb628c822654bef3dbe1
Nakayama Daiju <42.daiju@gmail.com>
2021-04-11 22:44:30 +0900

    camera を vfov とアスペクト比で作成

--- src/camera.rs
+++ src/camera.rs
@@ -2,9 +2,6 @@ use crate::{Ray, Vector};
 
 #[derive(Debug, Clone)]
 pub struct Camera {
-    pub viewport_height: f64,
-    pub viewport_width: f64,
-    pub focal_length: f64,
     origin: Vector,
     horizontal: Vector,
     vertical: Vector,
@@ -12,20 +9,16 @@ pub struct Camera {
 }
 
 impl Camera {
-    pub fn new(
-        origin: Vector,
-        viewport_height: f64,
-        viewport_width: f64,
-        focal_length: f64,
-    ) -> Camera {
+    pub fn new(vertical_field_of_view: f64, aspect_ratio: f64) -> Camera {
+        let h = (vertical_field_of_view.to_radians() * 0.5).tan();
+        let viewport_height = h * 2.0;
+        let viewport_width = viewport_height * aspect_ratio;
         let horizontal = Vector::new(viewport_width, 0.0, 0.0);
         let vertical = Vector::new(0.0, viewport_height, 0.0);
+        let origin = Vector::new(0.0, 0.0, 0.0);
         let lower_left_corner =
-            origin - horizontal / 2.0 - vertical / 2.0 - Vector::new(0.0, 0.0, focal_length);
+            origin - horizontal / 2.0 - vertical / 2.0 - Vector::new(0.0, 0.0, 1.0);
         Camera {
-            viewport_height,
-            viewport_width,
-            focal_length,
             origin,
             horizontal,
             vertical,
@@ -34,7 +27,7 @@ impl Camera {
     }
 
     pub fn aspect_ratio(&self) -> f64 {
-        self.viewport_width / self.viewport_height
+        self.horizontal.norm() / self.vertical.norm()
     }
 
     pub fn get_ray(&self, u: f64, v: f64) -> Ray {
@@ -47,7 +40,6 @@ impl Camera {
 
 impl Default for Camera {
     fn default() -> Camera {
-        let aspect_ratio = 16.0 / 9.0;
-        Camera::new(Vector::new(0.0, 0.0, 0.0), 2.0, 2.0 * aspect_ratio, 1.0)
+        Camera::new(90.0, 16.0 / 9.0)
     }
 }
